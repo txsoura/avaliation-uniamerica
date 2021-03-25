6e42c9d11edb2b141110ce5f5fed4f59b14704e0
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-03-24 22:27:36 -0300

    feat(repository): create brand repository

---
+++ src/main/java/com/uniamerica/avaliation/repository/BrandRepository.java
@@ -0,0 +1,8 @@
+package com.uniamerica.avaliation.repository;
+
+import com.uniamerica.avaliation.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {}
